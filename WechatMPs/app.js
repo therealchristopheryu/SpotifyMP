98c30679e2c567ab2ead61d389f79c5097edf9e9
Nathan Lee <kwnath@gmail.com>
2017-09-23 17:02:57 +0800

    Added user login

--- WechatMPs/app.js
+++ WechatMPs/app.js
@@ -1,48 +1,39 @@
 //app.js
-const AV = require('./libs/av-weapp-min.js');
+const AV = require('./utils/leancloud.js');
+const User = require('./models/user.js');
 
 // LeanCloud 应用的 ID 和 Key
 AV.init({
   appId: 'Ync2m3RPYwWg3vG7pbQzav4P-gzGzoHsz',
   appKey: 'gotmo3bIzYKQIclzqO4I18EW',
 });
-
-
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  let current_user = wx.getStorageSync('user')
+  // console.log('this is the locally stored user', user)
+  new AV.Query('Users')
+    .equalTo('objectId', current_user)
+    .find()
+    .then( function(res) {
+      if(res.length > 0){
+        console.log('user exists')
+      } else {
+        wx.getUserInfo({
+          success: function (res) {
+            const user = res.userInfo
+            // console.log(user.userInfo)
+            new User('Users', user)
+              .save()
+              .then(function (res) {
+                wx.setStorageSync('user', res.id)
+              })
+          }
+        })
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
   },
   globalData: {
-    userInfo: null
+    user: null
   }
 })
